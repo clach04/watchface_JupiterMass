07c2a77b4e9034e41954a1461fb5621b8e035764
clach04 <Chris.Clark@actian.com>
2015-06-14 15:25:13 -0700

    js code now handles cancell (or bad results)
    Push from CloudPebble.

--- src/pebble-js-app.js
+++ src/pebble-js-app.js
@@ -12,21 +12,33 @@ Pebble.addEventListener('showConfiguration', function(e) {
 });
 
 Pebble.addEventListener('webviewclosed',
-  function(e) {
-    var configuration = JSON.parse(decodeURIComponent(e.response));
-    var dictionary = {
-      "KEY_TIME_COLOR": parseInt(configuration.color, 16)
-    };
-    console.log('Color ' + configuration.color);
-    localStorage.setItem('color', configuration.color);
-    // Send to Pebble
-    Pebble.sendAppMessage(dictionary,
-      function(e) {
-        console.log("Configuration sent to Pebble successfully!");
-      },
-      function(e) {
-        console.log("Error sending configuration info to Pebble!");
-      }
-    );
-  }
+    function(e) {
+        console.log('e.response: ' + e.response);
+        console.log('e.response.length: ' + e.response.length);
+        try {
+            var configuration = JSON.parse(decodeURIComponent(e.response));
+            var dictionary = {
+              "KEY_TIME_COLOR": parseInt(configuration.color, 16)
+            };
+            console.log('Color ' + configuration.color);
+            localStorage.setItem('color', configuration.color);
+            // Send to Pebble
+            Pebble.sendAppMessage(dictionary,
+                function(e) {
+                    console.log("Configuration sent to Pebble successfully!");
+                },
+                function(e) {
+                    console.log("Error sending configuration info to Pebble!");
+                }
+            );
+        } catch (ex) {
+            // If we have SyntaxError JSON is invalid, anything unknown!?
+            if (ex instanceof SyntaxError) {
+                console.log('Probably Cancelled');
+            } else {
+                throw ex;
+                // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/SyntaxError
+            }
+        }
+    }
 );

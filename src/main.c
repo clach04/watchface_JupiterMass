0d65221012a639695e4321808694674fa47da2d8
clach04 <Chris.Clark@actian.com>
2015-09-01 21:28:28 -0700

    More font size tweaks

--- src/main.c
+++ src/main.c
@@ -33,7 +33,7 @@
 //#define BG_IMAGE RESOURCE_ID_IMAGE_N7LOGO_WHITE
 #define BG_IMAGE RESOURCE_ID_IMAGE_N7LOGO
 
-#define FONT_NAME RESOURCE_ID_FONT_JUPITER_ALPHABOT_XEN_80
+#define FONT_NAME RESOURCE_ID_FONT_JUPITER_ALPHABOT_XEN_75
 
 #define CLOCK_POS GRect(0, 52, 144, 168) /* probably taller than really needed */
 #define BT_POS GRect(0, 120, 144, 168) /* probably taller than really needed */

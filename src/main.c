e67f5b482b2cbf8178c4028e364854c495fa856e
clach04 <Chris.Clark@actian.com>
2015-09-02 17:34:33 -0700

    Move time up a few pixels
    Was cramped and BT disconnect was overlaying.
    
    NOTE also changed GUUI and name to allow this branch to be installed at the same time on same device as Master branch build. CloudPebble does NOT allow settings only commits hence this being mixed in time move change,

--- src/main.c
+++ src/main.c
@@ -35,7 +35,7 @@
 
 #define FONT_NAME RESOURCE_ID_FONT_JUPITER_ALPHABOT_XEN_75
 
-#define CLOCK_POS GRect(0, 52, 144, 168) /* probably taller than really needed */
+#define CLOCK_POS GRect(0, 40, 144, 168) /* probably taller than really needed */
 #define BT_POS GRect(0, 120, 144, 168) /* probably taller than really needed */
 #define DATE_POS GRect(0, 140, 144, 168) /* probably taller than really needed */
 #define BAT_POS GRect(0, 140, 144, 168) /* probably taller than really needed */

1a27512b0c623624b58e2a4e977915a8d531d088
clach04 <Chris.Clark@actian.com>
2015-09-04 20:47:57 -0700

    Move watch specific config into watch_config.h

---
+++ src/watch_config.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define USE_GENERIC_MAIN
+#undef REMOVE_LEADING_ZERO_FROM_TIME
+
+//#define BG_IMAGE RESOURCE_ID_IMAGE_N7LOGO_BLACK
+//#define BG_IMAGE RESOURCE_ID_IMAGE_N7LOGO_WHITE
+#define BG_IMAGE RESOURCE_ID_IMAGE_N7LOGO
+
+#define FONT_NAME RESOURCE_ID_FONT_JUPITER_60
+
+#define CLOCK_POS GRect(0, 52, 144, 168) /* probably taller than really needed */
+#define BT_POS GRect(0, 120, 144, 168) /* probably taller than really needed */
+#define DATE_POS GRect(0, 140, 144, 168) /* probably taller than really needed */
+#define BAT_POS GRect(0, 140, 144, 168) /* probably taller than really needed */
+
--- src/watchface.h
+++ src/watchface.h
@@ -1,19 +1,7 @@
 #pragma once
 
-#define USE_GENERIC_MAIN
-#undef REMOVE_LEADING_ZERO_FROM_TIME
-
-    //#define BG_IMAGE RESOURCE_ID_IMAGE_N7LOGO_BLACK
-//#define BG_IMAGE RESOURCE_ID_IMAGE_N7LOGO_WHITE
-#define BG_IMAGE RESOURCE_ID_IMAGE_N7LOGO
-
-#define FONT_NAME RESOURCE_ID_FONT_JUPITER_60
-
-#define CLOCK_POS GRect(0, 52, 144, 168) /* probably taller than really needed */
-#define BT_POS GRect(0, 120, 144, 168) /* probably taller than really needed */
-#define DATE_POS GRect(0, 140, 144, 168) /* probably taller than really needed */
-#define BAT_POS GRect(0, 140, 144, 168) /* probably taller than really needed */
-
+#include "watch_config.h"
+    
 /* PebbleKit JS, Message Keys, Pebble config keys */
 // FIXME why can't this be generated from the json settings file into a header?
 #define KEY_TIME_COLOR 0
